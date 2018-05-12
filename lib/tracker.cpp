b765dc6e66ac40ced6fbc91c10d3637403abfdb1
gr3yc4t <gr3yc4t@protonmail.com>
2018-05-12 23:44:47 +0200

    Remove HTTP Blocker

--- lib/tracker.cpp
+++ lib/tracker.cpp
@@ -55,7 +55,7 @@ namespace tracker{
             if(t_udp::is_udp_tracker(*it))
                 t_group.add_thread(new boost::thread(&t_udp::udp_manager, *it, *param));
             else
-                ;//t_group.add_thread(new boost::thread(process_tracker_request, *it, param, peer_list));
+                t_group.add_thread(new boost::thread(process_tracker_request, *it, param, peer_list));
         }
 
 
